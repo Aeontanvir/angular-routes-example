be8c09a5fb29279f58cc5a618dfcad21f778f403
aeontanvir <aeontanvir@gmail.com>
2018-05-05 00:13:51 +0600

    Route active class set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
+  styles: [`
+    a{
+      text-decoration: none;
+    }
+    a.active{
+      font-weight: bold;
+    }
+  `],
   template: `
     <nav>
-      <a routerLink="">Home</a>
-      <a routerLink="contacts">Contact</a>
+      <a routerLink=""
+      routerLinkActive="active"
+      [routerLinkActiveOptions]="{exact: true}"
+      >
+        Home
+      </a>
+      <a routerLink="contacts"
+      routerLinkActive="active"
+      >
+        Contact
+      </a>
     <nav>
     <router-outlet></router-outlet>
   `
